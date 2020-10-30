dd1354190034bb711848a4fecf57fe1c51897b58
Glitch (cyxkl-disappearing-ghost-starter) <none>
2020-10-30 00:28:58 +0000

    🎰👻 Checkpoint ./script.js:23568145/556

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
 
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
+
+$(".update-img-button").click(function() {
+  $(".ghost-image").attr("src", "https://pbs.twimg.com/profile_images/1247040888557076481/J0jP_aBA_400x400.jpg");
+});
+
+$(".message-button").click(function() {
+  $(".ghost-image").attr("src", "https://pbs.twimg.com/profile_images/1247040888557076481/J0jP_aBA_400x400.jpg");
+});
