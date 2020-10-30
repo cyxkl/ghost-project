632c6d2f46ac9ca2123aa1ab8aa9941b0481125f
Glitch (cyxkl-disappearing-ghost-starter) <none>
2020-10-30 00:51:57 +0000

    📼👵 Checkpoint ./script.js:23568145/280 ./index.html:23568145/1

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+//hide image
 $(".hide-button").click(function() {
   $(".ghost-image").hide();
 });
 
+//show image
 $(".show-button").click(function() {
   $(".ghost-image").show();
 });
 
+//change image
 $(".update-img-button").click(function() {
   $(".ghost-image").attr("src", "https://pbs.twimg.com/profile_images/1247040888557076481/J0jP_aBA_400x400.jpg");
 });
 
+//send image
 $(".message-button").click(function() {
   let message=$(".input").val()
   $(".message").append(message)
 });
 
+//name image
 $(".name-button").click(function() {
   let name=$(".input").val()
-  $(".namethig").html(name "is your name")
+  $(".namething").text(`Your name is ${name}`)
 });
